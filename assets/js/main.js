cde2e81319e19bc099a03b8090d9aaa5ccbe6ed7
mortenhostrup <hostrup97@icloud.com>
2018-04-16 10:26:30 +0200

    changed commentfunction

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,9 +20,13 @@ var sendbtn = document.querySelector(".commentbtn");
 var textarea = document.querySelector(".textcomment");
 var postContainer = document.querySelector(".post-container-subjects");
 
+var commentValue = [];
 function commentEvent() {
   sendbtn.addEventListener("click", function () {
-    localStorage.setItem("storecomment", textarea.value);
+
+    commentValue = JSON.parse(localStorage.getItem("storecomment")) || [];
+    commentValue.push(textarea.value);
+    localStorage.setItem("storecomment", JSON.stringify(commentValue));
 
     buildComment();
 
@@ -30,35 +34,39 @@ function commentEvent() {
 }
 
 function buildComment() {
+  var commentValue = JSON.parse(localStorage.getItem("storecomment"));
+
   if (localStorage.getItem("storecomment") === null ) {
     return;
   } else {
-    var divElm = document.createElement("div");
-    divElm.setAttribute("class", "comment-box");
-    var commentElm = document.createElement("div");
-    commentElm.setAttribute("class", "comment-user");
-    var commentName = document.createElement("h3");
-    commentName.innerHTML = "Anne-Sofie Nielsen";
-    var commentDate = document.createElement("p");
-    commentDate.innerHTML = "April 19, 2018";
-    commentElm.appendChild(commentName);
-    commentElm.appendChild(commentDate);
-    divElm.appendChild(commentElm);
+    for (var i = 0; i < commentValue.length; i++) {
+      var divElm = document.createElement("div");
+      divElm.setAttribute("class", "comment-box");
+      var commentElm = document.createElement("div");
+      commentElm.setAttribute("class", "comment-user");
+      var commentName = document.createElement("h3");
+      commentName.innerHTML = "Anne-Sofie Nielsen";
+      var commentDate = document.createElement("p");
+      commentDate.innerHTML = "April 19, 2018";
+      commentElm.appendChild(commentName);
+      commentElm.appendChild(commentDate);
+      divElm.appendChild(commentElm);
 
-    var kudoImg = document.createElement("img");
-    kudoImg.setAttribute("src", "assets/images/KUBOlogo.png");
-    kudoImg.setAttribute("class", "profile-pic");
+      var kudoImg = document.createElement("img");
+      kudoImg.setAttribute("src", "assets/images/KUBOlogo.png");
+      kudoImg.setAttribute("class", "profile-pic");
 
-    var contentElm = document.createElement("div");
-    contentElm.setAttribute("class", "comment-content");
-    var pElm = document.createElement("p");
-    var commentget = localStorage.getItem("storecomment");
-    pElm.innerHTML = commentget;
-    contentElm.appendChild(kudoImg);
-    contentElm.appendChild(pElm);
-    divElm.appendChild(contentElm);
+      var contentElm = document.createElement("div");
+      contentElm.setAttribute("class", "comment-content");
+      var pElm = document.createElement("p");
+      var commentget = commentValue[i];
+      pElm.innerHTML = commentget;
+      contentElm.appendChild(kudoImg);
+      contentElm.appendChild(pElm);
+      divElm.appendChild(contentElm);
 
-    postContainer.insertBefore(divElm, postContainer.firstChild);
+      postContainer.insertBefore(divElm, postContainer.firstChild);
+    }
   }
 }
 
@@ -77,7 +85,28 @@ function deleteClick() {
 function codingClick() {
   var codingButton = document.querySelector(".codingbtn");
   codingButton.addEventListener("click", function() {
-    lessonPlans();
+    lessonPlans("coding");
+  });
+}
+
+function mathClick() {
+  var mathButton = document.querySelector(".mathbtn");
+  mathButton.addEventListener("click", function() {
+    lessonPlans("math");
+  });
+}
+
+function scienceClick() {
+  var scienceButton = document.querySelector(".sciencebtn");
+  scienceButton.addEventListener("click", function() {
+    lessonPlans("science");
+  });
+}
+
+function spellingClick() {
+  var spellingButton = document.querySelector(".spellingbtn");
+  spellingButton.addEventListener("click", function() {
+    lessonPlans("spelling");
   });
 }
 
@@ -89,11 +118,11 @@ function allButtonClick() {
   });
 }
 
-function lessonPlans() {
+function lessonPlans(lesson) {
   var lessonplans = document.querySelectorAll(".lessonplan-box");
 
   for (var i = 0; i < lessonplans.length; i++) {
-    if (lessonplans[i].classList.contains("coding")) {
+    if (lessonplans[i].classList.contains(lesson)) {
       lessonplans[i].classList.add("showlesson");
       lessonplans[i].classList.remove("hidelesson");
     } else {
@@ -115,73 +144,76 @@ function buttonAll() {
   }
 }
 
-function newpostLocal () {
-  var titlePost = document.querySelector(".titel").value;
-  localStorage.setItem("postTitle", titlePost);
-}
-
+var publishValue = [];
 function publishButton() {
   var publishbtn = document.querySelector(".greenknap");
+
   publishbtn.addEventListener("click", function() {
-    newpostLocal();
+    var titlePost = document.querySelector(".titel").value;
+
+    publishValue = JSON.parse(localStorage.getItem("postTitle")) || [];
+    publishValue.push(titlePost);
+    localStorage.setItem("postTitle", JSON.stringify(publishValue));
   });
 }
 
 function newTopic() {
-  var postTitle = localStorage.getItem("postTitle");
+  var postTitle = JSON.parse(localStorage.getItem("postTitle"));
   var container = document.querySelector(".topic-container");
 
   if (postTitle === null) {
     return;
   } else {
-    var divElm = document.createElement("div");
-    divElm.setAttribute("class", "single-topic");
-    divElm.classList.add("gray-box");
+    for (var i = 0; i < postTitle.length; i++) {
+      var divElm = document.createElement("div");
+      divElm.setAttribute("class", "single-topic");
+      divElm.classList.add("gray-box");
 
-    var topicTitle = document.createElement("div");
-    topicTitle.setAttribute("class", "topic-title");
+      var topicTitle = document.createElement("div");
+      topicTitle.setAttribute("class", "topic-title");
 
-    divElm.appendChild(topicTitle);
+      divElm.appendChild(topicTitle);
 
-    var postImg = document.createElement("img");
-    postImg.setAttribute("src", "assets/images/KUBOlogo.png");
-    postImg.classList.add("profile-pic");
+      var postImg = document.createElement("img");
+      postImg.setAttribute("src", "assets/images/KUBOlogo.png");
+      postImg.classList.add("profile-pic");
 
-    topicTitle.appendChild(postImg);
+      topicTitle.appendChild(postImg);
 
-    var infoDiv = document.createElement("div");
-    var postHeadline = document.createElement("h3");
-    postHeadline.innerHTML = postTitle;
-    infoDiv.appendChild(postHeadline);
+      var infoDiv = document.createElement("div");
+      var postHeadline = document.createElement("h3");
+      postHeadline.innerHTML = postTitle[i];
+      infoDiv.appendChild(postHeadline);
 
-    var postText = document.createElement("p");
-    postText.innerHTML = "Anne-Sofie Nielsen, 19 April";
-    infoDiv.appendChild(postText);
-    topicTitle.appendChild(infoDiv);
+      var postText = document.createElement("p");
+      postText.innerHTML = "Anne-Sofie Nielsen, 19 April";
+      infoDiv.appendChild(postText);
+      topicTitle.appendChild(infoDiv);
 
-    container.appendChild(divElm);
+      container.appendChild(divElm);
 
-    var topicInfo = document.createElement("div");
-    topicInfo.setAttribute("class", "topic-info");
+      var topicInfo = document.createElement("div");
+      topicInfo.setAttribute("class", "topic-info");
 
-    var repliesTopic = document.createElement("div");
-    repliesTopic.setAttribute("class", "replies-topic");
-    var postReplies = document.createElement("p");
-    postReplies.innerHTML = "Replies: 0";
-    repliesTopic.appendChild(postReplies);
-    var postUpvotes = document.createElement("p");
-    postUpvotes.innerHTML = "Upvotes: 0";
-    repliesTopic.appendChild(postUpvotes);
-    topicInfo.appendChild(repliesTopic);
+      var repliesTopic = document.createElement("div");
+      repliesTopic.setAttribute("class", "replies-topic");
+      var postReplies = document.createElement("p");
+      postReplies.innerHTML = "Replies: 0";
+      repliesTopic.appendChild(postReplies);
+      var postUpvotes = document.createElement("p");
+      postUpvotes.innerHTML = "Upvotes: 0";
+      repliesTopic.appendChild(postUpvotes);
+      topicInfo.appendChild(repliesTopic);
 
-    var lastMessage = document.createElement("div");
-    lastMessage.setAttribute("class", "message-topic");
-    var messageP = document.createElement("p");
-    messageP.innerHTML = "0 Messages";
-    lastMessage.appendChild(messageP);
+      var lastMessage = document.createElement("div");
+      lastMessage.setAttribute("class", "message-topic");
+      var messageP = document.createElement("p");
+      messageP.innerHTML = "0 Messages";
+      lastMessage.appendChild(messageP);
 
-    topicInfo.appendChild(lastMessage);
-    divElm.appendChild(topicInfo);
+      topicInfo.appendChild(lastMessage);
+      divElm.appendChild(topicInfo);
+    }
   }
 }
 
